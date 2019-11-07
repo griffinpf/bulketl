853cc966e71d9d319bb029c6c8ecde44431bbdde
griffinpf <peteyg543@gmail.com>
2019-11-07 14:37:51 -0500

    Updated supporting docs

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# BulkETL
-
-BulkETL is a package to scrape a directory for all files that match a specified extension, then load them to a SQL Server back end.
+# bulketl
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md","r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["pandas>=0.24"]
+requirements = ["pandas>=0.24","tqdm>=4","SQLAlchemy>=1.3"]
 
 setup(
     name="bulketl",
